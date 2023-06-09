8c9ff3fad09780b7944c1ce1a25a8aaf77bac5de
addincw <addincendekia@gmail.com>
2023-06-09 21:51:35 +0700

    fix([theme-custom] folder structure): wrong require path file

--- classes/index.php
+++ classes/index.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once get_template_directory() . '/models/EventPostType.php';
+require_once get_template_directory() . '/classes/models/EventPostType.php';
--- inc/index.php
+++ inc/index.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once get_template_directory() . '/menu-functions.php';
+require_once get_template_directory() . '/inc/menu-functions.php';
