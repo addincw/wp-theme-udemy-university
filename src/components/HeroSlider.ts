5b5b495da34ae72eb96ed120036f1791e59d7410
addincw <addincendekia@gmail.com>
2023-06-16 16:09:17 +0700

    feat([javascript] hero slider): adding feature .hero-slider

---
+++ src/components/HeroSlider.ts
@@ -0,0 +1,34 @@
+import Glide, { Options } from "@glidejs/glide";
+
+class HeroSlider {
+  selector = ".hero-slider";
+
+  enable() {
+    const options: Partial<Options> = {
+      type: "carousel",
+      perView: 1,
+      autoplay: 3000,
+    };
+
+    new Glide(this.selector, options).mount();
+
+    this._addDotControls();
+  }
+
+  _addDotControls() {
+    const DotControls = document.querySelector(".glide__bullets");
+    const Slides = document.querySelectorAll(".hero-slider__slide");
+
+    Slides.forEach((Item, i) => {
+      if (Item.classList.contains("glide__slide--clone")) return;
+
+      const dotControl = document.createElement("button");
+      dotControl.classList.add("slider__bullet", "glide__bullet");
+      dotControl.setAttribute("data-glide-dir", `${i}`);
+
+      DotControls.insertAdjacentElement("beforeend", dotControl);
+    });
+  }
+}
+
+export default HeroSlider;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import HeroSlider from "./components/HeroSlider";
 import NavbarTopMobile from "./components/NavbarTopMobile";
+
+const heroSlider = new HeroSlider();
+heroSlider.enable();
 
 const navbarTopMobile = new NavbarTopMobile();
 navbarTopMobile.enable();
