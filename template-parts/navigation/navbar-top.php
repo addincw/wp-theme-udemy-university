bc33c9ce8143ceea629ae97d80234daa88ed2d00
addincw <addincendekia@gmail.com>
2023-07-06 16:47:45 +0700

    fix([permission] nav - login / logout): show login only for quest

--- template-parts/navigation/navbar-top.php
+++ template-parts/navigation/navbar-top.php
@@ -29,8 +29,19 @@
             </ul>
         </nav>
         <div class="site-header__util">
-            <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
-            <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+            <?php if (is_user_logged_in()) : ?>
+                <a href="#" class="btn btn--small btn--orange float-left push-right create-note-dialog__open">
+                    My Notes
+                </a>
+                <a href="<?php echo wp_logout_url(); ?>" class="btn btn--small  btn--dark-orange float-left btn--with-photo">
+                    <span class="site-header__avatar"><?php echo get_avatar(get_current_user_id(), 60); ?></span>
+                    <span class="btn__text">Log Out</span>
+                </a>
+            <?php else : ?>
+                <a href="<?php echo wp_login_url(); ?>" class="btn btn--small btn--orange float-left push-right">Login</a>
+                <a href="<?php echo wp_registration_url(); ?>" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+            <?php endif; ?>
+
             <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
         </div>
     </div>
